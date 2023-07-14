cfc0df08b8b29f9f463027ec7e052df4de28451a
Brian Michael <brianmichaeladero@gmail.com>
2023-07-14 03:42:46 +0300

    updated

---
+++ javascript learning/index.js
@@ -0,0 +1,28 @@
+let age =32;
+let username = 'Mike';
+let hobbies = ['sport', 'hacking', 'building', 'reading', 'cooking'];
+
+let job = {
+    title: "Developer",
+    place: "Nairobi",
+    salary: 8070274,
+};
+
+alert(hobbies[0]);
+alert(job.title);
+
+//functions
+let adultyears;
+
+function calculateadultyears(){
+   return age - 18;
+};
+
+adultyears=calculateadultyears();
+alert(adultyears);
+
+console.log(37/4)
+
+
+
+
