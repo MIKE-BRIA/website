f16e4c62b19dd896d9fb46d674de34bb8fe21af2
Brian Michael <brianmichaeladero@gmail.com>
2023-07-15 21:26:46 +0300

    updated on javascript

--- javascript learning/index.js
+++ javascript learning/index.js
@@ -2,6 +2,9 @@ let age =32;
 let username = 'Mike';
 let hobbies = ['sport', 'hacking', 'building', 'reading', 'cooking'];
 
+
+//object
+
 let job = {
     title: "Developer",
     place: "Nairobi",
@@ -21,13 +24,18 @@ function calculateadultyears(){
 adultyears=calculateadultyears();
 alert(adultyears);
 
-console.log(37/4)
+console.log(37/4);
 
+// objects
 
-//objects
+let person = {
+    name: CSSMathMax,//property
 
+    //function we do not have the function keyword if its in a object
 
+    greet(){
+        console.log("Hello everyone")//method
+    }
+};
 
-
-
-
+person.greet();
---
+++ javascript learning/operation.js
@@ -0,0 +1,26 @@
+// math operations
+
+console.log(40/4);
+console.log(10-4);
+console.log(10+4);
+console.log(40*4);
+console.log(4/5);
+console.log(10%4);//modulars operators
+
+//storing results of calculations in variables
+
+let results = 10* 4;
+results = results + 1; // results ++; // results +=1
+
+console.log(results);
+
+//concatenation of strings
+console.log("Brian" + " Michawel");
+
+//string operation and string methods
+
+let username = "Brian Michael";
+console.log(username.length);
+
+let hobbies = ['sports','swimming', 'cooking'];
+console.log(hobbies.length)
