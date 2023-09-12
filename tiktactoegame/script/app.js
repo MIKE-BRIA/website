788eaff4cf25e82df6043cf950d72b26e2b06bef
Brian Michael <brianmichaeladero@gmail.com>
2023-09-12 06:40:33 +0300

    variables and eventlistners

--- tiktactoegame/script/app.js
+++ tiktactoegame/script/app.js
@@ -14,11 +14,14 @@ const players = [
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backDrop = document.getElementById("backdrop");
 const formElement = document.querySelector('form');
-const errorsOutput = document.getElementById('config-errors')
+const errorsOutput = document.getElementById('config-errors');
+const gameArea = document.getElementById('active-game');
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+const startNewgame = document.getElementById('start-game-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -27,3 +30,4 @@ cancelBtn.addEventListener('click', closePlayerConfig);
 backDrop.addEventListener('click', closePlayerConfig)
 
 formElement.addEventListener('submit', savePlayerConfig)
+startNewgame.addEventListener('click', StartNewGame);
