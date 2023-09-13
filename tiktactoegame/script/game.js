1b527bc7f8fe9c0a22015baf75c16e3b7f9811ad
Brian Michael <brianmichaeladero@gmail.com>
2023-09-13 05:50:04 +0300

    functions for the game section

--- tiktactoegame/script/game.js
+++ tiktactoegame/script/game.js
@@ -4,5 +4,26 @@ function StartNewGame(){
         return;
     }
 
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameArea.style.display = 'block';
+}
+
+function switchPlayer(){
+    if (activePlayer === 0){
+        activePlayer = 1;
+    }else{
+        activePlayer = 0;
+    }
+
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event){
+    if (event.target.tagName !== 'LI'){
+        return;
+    }
+
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add('disabled');
+    switchPlayer();
 }
