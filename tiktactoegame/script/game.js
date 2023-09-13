fe69f63ffcc3f973a4e71a52917a3b03c651f965
Brian Michael <brianmichaeladero@gmail.com>
2023-09-13 15:17:48 +0300

    functions for the game section

--- tiktactoegame/script/game.js
+++ tiktactoegame/script/game.js
@@ -20,10 +20,74 @@ function switchPlayer(){
 
 function selectGameField(event){
     if (event.target.tagName !== 'LI'){
+        alert("click on an empty field")
         return;
     }
 
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add('disabled');
+    const selectedField = event.target
+    const selectedColumn = selectedField.dataset.col -1;
+    const selectedRow = selectedField.dataset.row -1;
+
+    if (gameData[selectedRow][selectedColumn] > 0){
+        return;
+    }
+
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add('disabled'); 
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+    const winnerId = checkForGameOver();
+
+    currentRound++;
     switchPlayer();
+}
+
+function checkForGameOver(){
+    //Checking the rows for equality
+    for (let i = 0; i < 3; i++){
+        if(
+            gameData[i][0] > 0 &&
+            gameData[i][0] === gameData[i][1] &&
+            gameData[i][1] === gameData[i][2]
+        ){
+            return gameData[i][0];
+        }
+    }
+
+    //Checking the columns for equality
+    for (let i = 0; i < 3; i++){
+        if(
+            gameData[0][i] > 0 &&
+            gameData[0][i] === gameData[1][i] &&
+            gameData[0][i] === gameData[2][i]
+        ){
+            return gameData[0][i];
+        }
+    }
+
+    //Diagonal: Top left to bottom right
+    if (
+        gameData[0][0] > 0 &&
+        gameData[0][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[2][2]
+    ){
+        return gameData[0][0];
+    }
+
+    //Diagonal: Bottom left to Top right
+    if (
+        gameData[2][0] > 0 &&
+        gameData[2][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[0][2]
+    ){
+        return gameData[2][0];
+    }
+
+
+    if (currentRound === 9){
+        return -1;
+    }
+
+    return 0;
 }
